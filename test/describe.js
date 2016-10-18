ec1a68578dd006a2b153a032361e5d1b50d1e389
Simon Ho <simonho@ca.ibm.com>
2016-10-17 22:59:52 -0700

    Refactor and enable tests
    - Enable tests by removing test/describe.js
    - Move tests into test/integration
    - Modify initialize to trigger callback correctly after connecting
      successfully (was causing tests to hang)
    - Refactor test initialization to use expect and config globally
    - Refactor suites to setup/teardown tests properly
    - Remove should in favour of chai/dirty-chai
    - Remove unused devdeps blueblird and sinon
    - Add semver to check for node 0.10 syntax for inheriting EventEmitter
    - Increase timeout to 10000ms
    - Skip tests on Node version 6 (not supported by underlying driver
      mqlight
    - Fail the connector gracefully on initalize via callback when Node 6 is
      used (instead of throwing on require which causes the app the crash)

--- test/describe.js
+++ test/describe.js
@@ -1,3 +1,6 @@
 'use strict';
+var semver = require('semver');
 
-module.exports = process.env.CI ? describe.skip.bind(describe) : describe;
+// Skip all tests on Node 6 because MQLight does not support it
+var runningOnNode6 = semver.satisfies(process.version, '^6.x.x');
+module.exports = runningOnNode6 ? describe.skip.bind(describe) : describe;
