51e0ef8fc3f93072fde112b1cd33edf871738513
Simon Ho <superkhau@gmail.com>
2016-10-18 22:48:15 -0700

    Refactor and enable tests (#22)
    - Enable tests by removing test/describe.js
    - Move tests into test/integration
    - Modify initialize to trigger callback correctly after connecting
      successfully (was causing tests to hang)
    - Refactor test initialization to use expect and config globally
    - Refactor suites to setup/teardown tests properly
    - Remove should in favour of chai/dirty-chai
    - Remove unused devdeps blueblird and sinon
    - Add semver to check for node 0.10 syntax for inheriting EventEmitter
    - Increase timeout to 10000ms
    - Skip tests on Node version 6 (not supported by underlying driver
      mqlight
    - Fail the connector gracefully on initalize via callback when Node 6 is
      used (instead of throwing on require which causes the app the crash)
    - Add missing globalization files
    
    Backport of #22

---
+++ test/integration/messaging.js
@@ -0,0 +1,74 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-mqlight
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+var connector = require('../..');
+var DataSource = require('loopback-datasource-juggler').DataSource;
+var describe = require('../describe');
+
+describe('messaging', function() {
+  var sender, receiver, senderModel, receiverModel;
+  beforeEach(setupSender);
+  beforeEach(setupReceiver);
+  afterEach(teardownSender);
+  afterEach(teardownReceiver);
+
+  it('creates a message', function(done) {
+    senderModel.create({topic: 'public', message: 'A create message'},
+    function(err) {
+      if (err) return done(err);
+      receiverModel.find(0, function(msg) {
+        expect(msg).to.equal('A create message');
+        done();
+      });
+    });
+  });
+
+  it('updates a message', function(done) {
+    senderModel.update({topic: 'public', message: 'An update message'},
+    function(err) {
+      if (err) return done(err);
+      receiverModel.find(0, function(msg) {
+        expect(msg).to.equal('An update message');
+        done();
+      });
+    });
+  });
+
+  it('deletes a message', function(done) {
+    senderModel.delete({topic: 'public', message: 'A delete message'},
+    function(err) {
+      if (err) return done(err);
+      receiverModel.find(0, function(msg) {
+        expect(msg).to.equal('A delete message');
+        done();
+      });
+    });
+  });
+
+  function setupSender(done) {
+    sender = new DataSource(connector, config);
+    sender.on('started', function() {
+      senderModel = sender.createModel('senderModel', {});
+      done();
+    });
+  }
+  function setupReceiver(done) {
+    receiver = new DataSource(connector, config);
+    receiver.on('started', function() {
+      receiverModel = receiver.createModel('receiverModel', {});
+      receiverModel.subscribe('public');
+      done();
+    });
+  }
+  function teardownSender() {
+    if (sender.connected)
+      sender.disconnect();
+  }
+  function teardownReceiver() {
+    if (receiver.connected)
+      receiver.disconnect();
+  }
+});
